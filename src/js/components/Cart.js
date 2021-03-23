a6fe8605c267eada2830de110dbebbbddb2d8481
Anna Szwal <aniaszwal@gmail.com>
2021-03-23 12:06:01 +0100

    almost fixed files

--- src/js/components/Cart.js
+++ src/js/components/Cart.js
@@ -1,6 +1,6 @@
-import{select, classNames, templates, settings} from './settings.js';
-import CartProduct from './components/CartProduct';
-import utils from './utils.js';
+import{select, classNames, templates, settings} from '../settings.js';
+import CartProduct from './CartProduct';
+import utils from '../utils.js';
 
 class Cart {
   constructor(element) {
